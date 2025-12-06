d0a4ba43c89a5b9588bc3e90a1c14e84b3f980e9
littleseedlimited <littleseedlimited@gmail.com>
2025-12-06 02:24:50 +0100

    Add files via upload

--- mini_app/js/app.js
+++ mini_app/js/app.js
@@ -365,7 +365,14 @@ function setupScrollAnimations() {
         el.style.transform = 'translateY(20px)';
         el.style.transition = 'opacity 0.5s ease-out, transform 0.5s ease-out';
         observer.observe(el);
-    });
+    }); \
+}
+
+function setupEventListeners() {
+    // Setup tab navigation
+    setupTabNavigation();
+
+    // Add any other event listeners here
 }
 
 function setupTabNavigation() {
